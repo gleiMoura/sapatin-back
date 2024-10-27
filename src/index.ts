bc725e79fe946940319815e33b07b55d67793388
gleiMoura <gle.bar.mou@gmail.com>
2024-10-27 10:37:26 -0300

    ♻️ refactor: improve the text to show port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import db from "config";
 dotenv.config();
 
 app.listen(process.env.PORT || "5000", () => {
-    console.log("Server is running!");
+    console.log("Server is running in port 5000!");
 });
 
 db //show that database is working in node!
