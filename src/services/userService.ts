f648ddcd03bdfae0200474d39a6ad60b87b5f9aa
gleiMoura <gle.bar.mou@gmail.com>
2024-10-30 18:10:18 -0300

    ✨ feat: create a service to save user adress

---
+++ src/services/userService.ts
@@ -0,0 +1,18 @@
+import { adressType } from "interfaces";
+import { findUserByToken } from "repository/userRepository";
+import { saveUserAdress } from "repository/userRepository";
+
+export const saveAdress = async (token: string | string[], adress: adressType) => {
+    const user = await findUserByToken(token);
+
+    if (!user) {
+        throw {
+            response: {
+                status: 404,
+                message: "User is not logged"
+            }
+        }
+    };
+
+    await saveUserAdress(user, adress);
+};
