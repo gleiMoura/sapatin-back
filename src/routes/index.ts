f83612690e4f7694b2f307ce19e16e0a1fbe144d
gleiMoura <gle.bar.mou@gmail.com>
2024-11-06 21:05:20 -0300

    ✨ feat: use product router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import Router from "express";
 import loginRouter from "./loginRouter";
 import registerRouter from "./registerRouter";
 import userRouter from "./userRouter";
+import productsRouter from "./productsRouter";
 
 const router = Router();
 router.use(loginRouter);
 router.use(registerRouter);
 router.use(userRouter);
+router.use(productsRouter)
 
 export default router;
