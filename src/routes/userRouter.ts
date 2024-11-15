bb361c5d279502bd583721ecf783671f3917b24d
gleiMoura <gle.bar.mou@gmail.com>
2024-11-15 19:25:41 -0300

    ✨ feat: create a route to get adress data

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import schemaValidator from "middlewares/schemaValidator";
 import adressSchema from "schemas/adressSchema";
-import { insertAdress } from "controllers/userController";
+import { getAdress, insertAdress } from "controllers/userController";
 
 const userRouter = Router();
 
 userRouter.post("/user/adress", schemaValidator(adressSchema), insertAdress);
+userRouter.get("/user/adress", getAdress)
 
 export default userRouter;
