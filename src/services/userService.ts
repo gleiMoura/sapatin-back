c0586d5b9a6659e677dfa5155fd8a82ff38d6b3b
gleiMoura <gle.bar.mou@gmail.com>
2024-11-15 19:26:20 -0300

    ✨ feat: create a service to get adress data

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,5 @@
 import { adressType } from "interfaces";
-import { findUserByToken } from "repository/userRepository";
+import { findUserByToken, getAdressFromDb } from "repository/userRepository";
 import { saveUserAdress } from "repository/userRepository";
 
 export const saveAdress = async (token: string | string[], adress: adressType) => {
@@ -16,3 +16,18 @@ export const saveAdress = async (token: string | string[], adress: adressType) =
 
     await saveUserAdress(user.email, adress);
 };
+
+export const getUserAdress = async (token: string | string[]) => {
+    const user = await findUserByToken(token);
+
+    if (!user) {
+        throw {
+            response: {
+                status: 404,
+                message: "User is not logged"
+            }
+        }
+    };
+
+    return await getAdressFromDb(user.email);
+}
