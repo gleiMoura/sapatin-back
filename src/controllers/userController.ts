c496ee5272017a6be07926fb299b32ddf95a6071
gleiMoura <gle.bar.mou@gmail.com>
2024-11-15 19:24:46 -0300

    🐛 fix: working with token properly

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from "express";
 import { adressType } from "interfaces";
-import { saveAdress } from "services/userService";
+import { getUserAdress, saveAdress } from "services/userService";
 
 export const insertAdress = async (req: Request, res: Response) => {
     const adress: adressType = req.body;
-    const { token } = req.headers;
+    const { authorization } = req.headers;
+    const token = authorization.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).send("Token is missing or invalid!")
+    }
 
     await saveAdress(token, adress);
 
-    res.status(200).send("Successfully saved!")
+    res.status(200).send(adress)
+};
+
+export const getAdress = async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    const token = authorization.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).send("Token is missing or invalid!")
+    }
+
+    const adress = await getUserAdress(token);
+
+    res.status(200).send(adress);
 }
