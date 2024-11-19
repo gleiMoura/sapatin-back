e14868441bb7920382b4633a649aa7f5e702a045
gleiMoura <gle.bar.mou@gmail.com>
2024-11-19 15:41:29 -0300

    ✨ feat: create the repository generateProductLink to add a new link image to a product

--- src/repository/productRepository.ts
+++ src/repository/productRepository.ts
@@ -1,7 +1,6 @@
-import { GridFSBucketWriteStream, ObjectId } from "mongodb";
 import { productType } from "interfaces";
 import config from "config";
-import { response } from "express";
+import { bucket, bucketName } from "config/cloud";
 
 const { db } = config;
 
@@ -11,12 +10,12 @@ export const insertProductInDb = async (productInfo: productType) => {
             throw {
                 response: {
                     status: 400,
-                    message: "it is not possible insert withou product information!"
+                    message: "it is not possible insert without product information!"
                 }
             }
         };
 
-        const products = await db.collection("shoes").insertOne(productInfo);
+        const products = await db.collection("products").insertOne(productInfo);
 
         return products
     } catch (error) {
@@ -26,7 +25,7 @@ export const insertProductInDb = async (productInfo: productType) => {
 
 export const insertManyProductInDb = async (productsInfo: productType[]) => {
     try {
-        const products = await db.collection("shoes").insertMany(productsInfo);
+        const products = await db.collection("products").insertMany(productsInfo);
 
         return products
     } catch (error) {
@@ -36,10 +35,49 @@ export const insertManyProductInDb = async (productsInfo: productType[]) => {
 
 export const getProductsFromDb = async () => {
     try {
-        const products = await db.collection("shoes").find().toArray();
+        const products = await db.collection("products").find().toArray();
         return products;
     } catch (error) {
         console.log("Error trying get products from db");
     }
+};
 
+export const generateProductLink = async (file) => {
+    try {
+        const filePath = file.path;
+        const fileName = file.originalname;
+        const destination = bucket.file(fileName);
+
+        // Enviando o arquivo para o Google Cloud Storage
+        await bucket.upload(filePath, {
+            destination: fileName,
+        });
+
+        // Tornando o arquivo público
+        await destination.makePublic();
+
+        const publicUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+        console.log(publicUrl)
+        return publicUrl;
+    } catch (error) {
+        console.error('Erro no upload:', error);
+    }
+};
+
+export const getProductByLink = async (link: string) => {
+    try {
+        if (!link) {
+            throw {
+                response: {
+                    status: 400,
+                    message: "It's necessary create link"
+                }
+            }
+        };
+
+        const product = await db.collection("products").findOne({ url: link });
+        return product;
+    } catch (error) {
+
+    }
 }
