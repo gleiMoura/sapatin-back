5239591a93dec21788023d695d524ee75c8edc61
gleiMoura <gle.bar.mou@gmail.com>
2024-11-19 15:41:52 -0300

    ✨ feat: create the router to add a product with a file repository

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
-import { PutManyProducts, PutSingleProduct } from "controllers/productsController";
+import { PutManyProducts, putProductAsFile, PutSingleProduct } from "controllers/productsController";
 import { getProducts } from "controllers/productsController";
-import multer from "multer";
 import schemaValidator from "middlewares/schemaValidator";
 import productsSchema from "schemas/productsSchema";
 import manyProductsSchema from "schemas/manyProductsSchema";
+import multer from "multer";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ dest: 'uploads/' });
 
 const productsRouter = Router();
 
 productsRouter.post("/product/upload", schemaValidator(productsSchema), PutSingleProduct);
-productsRouter.post("/products/upload", schemaValidator(manyProductsSchema), PutManyProducts)
+productsRouter.post("/products/upload/many", schemaValidator(manyProductsSchema), PutManyProducts)
 productsRouter.get("/products/upload", getProducts);
+productsRouter.post("/products/upload/file", upload.single('file'), putProductAsFile)
 
 export default productsRouter;
