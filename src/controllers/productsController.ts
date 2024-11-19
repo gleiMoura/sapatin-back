22029003aa92eab8c0adb7cc2cc67bcf16688699
gleiMoura <gle.bar.mou@gmail.com>
2024-11-19 15:40:04 -0300

    ✨ feat: create controller to insert product with file image

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { productType } from "interfaces";
+import { generateProductLink } from "repository/productRepository";
 import { getAllProducts, insertManyProducts, insertProduct } from "services/productsService";
 
 export const PutSingleProduct = async (req: Request, res: Response) => {
@@ -22,4 +23,15 @@ export const getProducts = async (req: Request, res: Response) => {
     const products = await getAllProducts();
 
     res.status(201).send(products);
+};
+
+export const putProductAsFile = async (req: Request, res: Response) => {
+    const file = req.file;
+    const productInfo: productType = req.body;
+
+    const productLink = await generateProductLink(file);
+
+    const product = await insertProduct({ ...productInfo, url: productLink });
+
+    res.status(201).send(product);
 };
